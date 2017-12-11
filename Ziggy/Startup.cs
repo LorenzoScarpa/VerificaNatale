535cc7e01a84ba68ff9e2b8988601bf91f579447
LorenzoScarpa <lorenzo.scarpa@edu.itspiemonte.it>
2017-12-11 09:35:06 +0100

    Aggiungere i file di progetto.

---
+++ Ziggy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ziggy.Startup))]
+namespace Ziggy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
